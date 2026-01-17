3a120a75214f14ef9fe51dc2030e946060b7bf4e
ZeroToHeroTD <easylynx9@gmail.com>
2026-01-17 18:24:16 +0800

    yes

--- birthday project/script.js
+++ birthday project/script.js
@@ -2,6 +2,7 @@ function startGalaxyAnimation() {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
+
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
